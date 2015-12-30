b552e38618023e3a28fac3c464def9b0cfaa740c
Andrew Valencik <andrew.valencik@gmail.com>
2015-12-30 00:18:14 -0400

    Switched to routes, added users/id route

--- lib/helloplug.ex
+++ lib/helloplug.ex
@@ -5,8 +5,22 @@ defmodule Helloplug do
   end
 
   def call(conn, _opts) do
-    IO.puts "saying hello!"
-    # Last statement of a function is returned
-    conn |> Plug.Conn.put_resp_header("Server", "Plug") |> Plug.Conn.send_resp(200, "Hello, World!")
+    route(conn.method, conn.path_info, conn)
   end
+
+  def route("GET", ["hello"], conn) do
+    # this route is for /hello
+    conn |> Plug.Conn.send_resp(200, "Hello World")
+  end
+
+  def route("GET", ["users", user_id], conn) do
+    # this route is for /users/<user_id>
+    conn |> Plug.Conn.send_resp(200, "You requested user #{user_id}")
+  end
+
+  def route(_method, _path, conn) do
+    # this route is called if no other routes match
+    conn |> Plug.Conn.send_resp(404, "Couldn't find that page, sorry!")
+  end
+
 end

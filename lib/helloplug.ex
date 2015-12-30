28493b119adcef199a97b33972fb71ab12078623
Andrew Valencik <andrew.valencik@gmail.com>
2015-12-30 00:48:00 -0400

    Added basic eex template

--- lib/helloplug.ex
+++ lib/helloplug.ex
@@ -15,7 +15,8 @@ defmodule UserRouter do
   use Router
   def route("GET", ["users", user_id], conn) do
     # this route is for /users/<user_id>
-    conn |> Plug.Conn.send_resp(200, "You requested user #{user_id}")
+    page_contents = EEx.eval_file("templates/show_user.eex", [user_id: user_id])
+    conn |> Plug.Conn.put_resp_content_type("text/html") |> Plug.Conn.send_resp(200, page_contents)
   end
 
   def route(_method, _path, conn) do

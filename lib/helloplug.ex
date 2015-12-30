6040b8e7b6910b125b740d513ce0920549ab6df3
Andrew Valencik <andrew.valencik@gmail.com>
2015-12-30 00:29:41 -0400

    Added Router macro

--- lib/helloplug.ex
+++ lib/helloplug.ex
@@ -1,13 +1,18 @@
+defmodule Router do
+  defmacro __using__(_opts) do
+    quote do
+      def init(options) do
+        options
+      end
+      def call(conn, _opts) do
+        route(conn.method, conn.path_info, conn)
+      end
+    end
+  end
+end
+
 defmodule Helloplug do
-  def init(default_opts) do
-    IO.puts "starting up Helloplug..."
-    default_opts
-  end
-
-  def call(conn, _opts) do
-    route(conn.method, conn.path_info, conn)
-  end
-
+  use Router
   def route("GET", ["hello"], conn) do
     # this route is for /hello
     conn |> Plug.Conn.send_resp(200, "Hello World")

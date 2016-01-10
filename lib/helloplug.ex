a07fdea5970675db5cb8329787e0c4abfb7db3a8
Andrew Valencik <andrew.valencik@gmail.com>
2016-01-10 11:17:21 -0400

    Added Ecto model (broken)
    Added an Ecto model following the code examples from:
    https://codewords.recurse.com/issues/five/building-a-web-framework-from-scratch-in-elixir

--- lib/helloplug.ex
+++ lib/helloplug.ex
@@ -1,3 +1,19 @@
+defmodule Helloplug.Repo do
+  use Ecto.Repo,
+    otp_app: :helloplug,
+    adapter: Sqlite.Ecto
+end
+
+defmodule User do
+  use Ecto.Model
+
+  schema "users" do
+    # id field is implicit
+    field :first_name, :string
+    field :last_name, :string
+  end
+end
+
 defmodule Router do
   defmacro __using__(_opts) do
     quote do
@@ -14,21 +30,20 @@ end
 defmodule UserRouter do
   use Router
   require EEx # you have to require EEx before using its macros outside of functions
-  EEx.function_from_file :defp, :template_show_user, "templates/show_user.eex", [:user_id]
+  EEx.function_from_file :defp, :template_show_user, "templates/show_user.eex", [:user]
 
   def route("GET", ["users", user_id], conn) do
-    # this route is for /users/<user_id>
-    page_contents = template_show_user(user_id)
-    conn |> Plug.Conn.put_resp_content_type("text/html") |> Plug.Conn.send_resp(200, page_contents)
-  end
-
-  def route(_method, _path, conn) do
-    # this route is called if no other routes match
-    conn |> Plug.Conn.send_resp(404, "Couldn't find that user page, sorry!")
+    case Helloplug.Repo.get(User, user_id) do
+      nil ->
+        conn |> Plug.Conn.send_resp(404, "User with that ID not found, sorry.")
+      user ->
+        page_contents = template_show_user(user)
+        conn |> Plug.Conn.put_resp_content_type("text/html") |> Plug.Conn.send_resp(200, page_contents)
+    end
   end
 end
 
-defmodule WebsiteRouter do
+defmodule Helloplug do
   use Router
 
   @user_router_options UserRouter.init([])
